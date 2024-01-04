3f9e8f6343da338123b9b0af6f892f32fb064e00
Joshua <76941328+joshua-legend@users.noreply.github.com>
2024-01-04 12:49:37 +0900

    Add files via upload

---
+++ day02/operator.py
@@ -0,0 +1,18 @@
+#operator
+
+# print(),input(),int(),str(),float(),
+# type():타입 알려주는 기능
+# print(type('123')) #<class 'str'>
+# print(type(123)) #<class 'int'>
+
+#숫자 사칙 연산자
+# +,-,*
+# /(나눗셈 연산자) , %(나머지 연산자)
+print(5 / 2) # 2.5
+print(5 % 2) # 1
+
+# //(정수 나머지[몫]), **(제곱 연산자)
+print(5 // 2) # 2
+print(2 ** 4) # 16
+
+
---
+++ day02/review.py
@@ -0,0 +1,9 @@
+time = input("오늘 일어난 시간:")
+food = input("오늘 아침 메뉴:")
+
+# 연산(operator)
+# 1+2+3 : 사칙 연산
+# time : 호출 연산
+# and or not: 논리 연산
+
+print(f"오늘 기상 시간은 {time}이군요! 오늘 먹은 아침은 {food} 이시네요!")
