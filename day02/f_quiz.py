86a15ef89e031491e1bc3900ed6b5c9d66993296
Joshua <76941328+joshua-legend@users.noreply.github.com>
2024-02-01 10:46:51 +0900

    Add files via upload

---
+++ day02/f_quiz.py
@@ -0,0 +1,19 @@
+#f_quiz
+
+# food = input("당신의 최애 음식은")
+# animal = input("당신의 최애 동물은")
+# mbti = input("당신의 mbti는")
+# address = input("당신의 사는 곳은")
+#
+# print(f"당신이 좋아하는 음식은 {food}이고, 최애 동물은 {animal} 이군요!")
+# print(f"당신의 mbti는 {mbti}이시고, 사는곳은 {address}이신군요!")
+
+first = input("첫 번째 숫자 입력:")
+second = input("두 번째 숫자 입력:")
+print(f"두 수의 합은 {first+second}입니다.")
+
+
+
+
+
+
--- day02/review.py
+++ day02/review.py
@@ -1,9 +1,8 @@
-time = input("오늘 일어난 시간:")
-food = input("오늘 아침 메뉴:")
-
-# 연산(operator)
-# 1+2+3 : 사칙 연산
-# time : 호출 연산
-# and or not: 논리 연산
-
-print(f"오늘 기상 시간은 {time}이군요! 오늘 먹은 아침은 {food} 이시네요!")
+# print("오늘 아침 날씨 좋음")
+# input("오늘 아침 몇시 기상?")
+wake = input("오늘 기상 시간 입력:")
+meal = input("오늘 아침 식사 입력:")
+time = input("오늘 아침 출근 시간 입력:")
+print(wake)
+print(meal)
+print(time)
